f0278ccef6a7edd26d58a0e5eeedcaae7cea022f
ProbablyRaging <probablyraging@gmail.com>
2026-05-26 15:39:02 +1000

    refactor: change splash screen bg color and fix white margin

--- src/stremio_app/splash.rs
+++ src/stremio_app/splash.rs
@@ -8,7 +8,7 @@ pub struct SplashImage {
     embed: nwg::EmbedResource,
     #[nwg_resource(size: Some((300,300)), source_embed: Some(&data.embed), source_embed_str: Some("SPLASHIMAGE"))]
     splash_image: nwg::Bitmap,
-    #[nwg_layout(spacing: 0, min_size: [WINDOW_MIN_WIDTH as u32, WINDOW_MIN_HEIGHT as u32])]
+    #[nwg_layout(spacing: 0, margin: [0,0,0,0], min_size: [WINDOW_MIN_WIDTH as u32, WINDOW_MIN_HEIGHT as u32])]
     grid: nwg::GridLayout,
     #[nwg_control(background_color: Some(Self::BG_COLOR), bitmap: Some(&data.splash_image))]
     #[nwg_layout_item(layout: grid, col: 0, row: 0)]
@@ -16,7 +16,7 @@ pub struct SplashImage {
 }
 
 impl SplashImage {
-    const BG_COLOR: [u8; 3] = [27, 17, 38];
+    const BG_COLOR: [u8; 3] = [0x1e, 0x1e, 0x1e];
     pub fn visible(&self) -> bool {
         self.splash.visible()
     }
